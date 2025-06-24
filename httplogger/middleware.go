ec71abdfb4b4d45aa57319f935776851b101a9be
Anton Bronnikov <anton@northernforest.nl>
2025-06-24 18:04:00 +0200

    chore: debug-log `x-forwarded-for` header

--- httplogger/middleware.go
+++ httplogger/middleware.go
@@ -48,13 +48,14 @@ func Middleware(logger *zap.Logger, next http.Handler) http.Handler {
 		// Passing request stats both in-message (for the human reader)
 		// as well as inside the structured log (for the machine parser)
 		logger.Debug(fmt.Sprintf("%s %s %d", r.Method, r.URL.EscapedPath(), wrapped.Status()),
-			zap.Int("durationMs", int(time.Since(start).Milliseconds())),
+			zap.Int("duration_ms", int(time.Since(start).Milliseconds())),
 			zap.Int("status", wrapped.Status()),
-			zap.String("httpRequestID", httpRequestID),
-			zap.String("logType", "access"),
+			zap.String("http_request_id", httpRequestID),
+			zap.String("log_type", "access"),
 			zap.String("method", r.Method),
 			zap.String("path", r.URL.EscapedPath()),
-			zap.String("userAgent", r.Header.Get("user-agent")),
+			zap.String("forwarded_for", r.Header.Get("x-forwarded-for")),
+			zap.String("user_agent", r.Header.Get("user-agent")),
 		)
 	})
 }
